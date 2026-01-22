b69555b1813ebcc0a849f7829197bf17e18b1772
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2026-01-23 02:31:50 +0900

    "자식"측 상속 매핑 추가

--- src/main/java/com/lhj/myoppingmall/item/entity/category/ElectronicDevice.java
+++ src/main/java/com/lhj/myoppingmall/item/entity/category/ElectronicDevice.java
@@ -2,7 +2,11 @@ package com.lhj.myoppingmall.item.entity.category;
 
 import com.lhj.myoppingmall.item.entity.Item;
 
-public class ElectronicDevice extends Item{
+@Entity
+@Table(name = "electronicDevices")
+@DiscriminatorValue("ELECTRONIC_DEVICE")
+@PrimaryKeyJoinColumn(name = "item_id")
+public class ElectronicDevice extends Item {
 
     private String manufacturerCompany;
     private String description;
