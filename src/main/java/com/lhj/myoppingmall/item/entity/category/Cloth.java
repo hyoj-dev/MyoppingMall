862e345a906e588a606d9f7ab23aee04a2d3aaed
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2026-01-23 02:28:52 +0900

    "자식"측 상속 매핑 추가

--- src/main/java/com/lhj/myoppingmall/item/entity/category/Cloth.java
+++ src/main/java/com/lhj/myoppingmall/item/entity/category/Cloth.java
@@ -2,7 +2,10 @@ package com.lhj.myoppingmall.item.entity.category;
 
 import com.lhj.myoppingmall.item.entity.Item;
 
-
+@Entity
+@Table(name = "clothes")
+@DiscriminatorValue("CLOTH")
+@PrimaryKeyJoinColumn(name = "item_id")
 public class Cloth extends Item {
     
     private int size;
