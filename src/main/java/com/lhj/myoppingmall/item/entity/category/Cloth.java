38fffc1a0e023bddce89201fffe3120da0be2f3e
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2026-01-23 02:27:10 +0900

    상속 구조 "자식"의 필드 수정

--- src/main/java/com/lhj/myoppingmall/item/entity/category/Cloth.java
+++ src/main/java/com/lhj/myoppingmall/item/entity/category/Cloth.java
@@ -4,9 +4,7 @@ import com.lhj.myoppingmall.item.entity.Item;
 
 
 public class Cloth extends Item {
-
-    private Item item;
-
+    
     private int size;
     private String brand;
     private String description;
