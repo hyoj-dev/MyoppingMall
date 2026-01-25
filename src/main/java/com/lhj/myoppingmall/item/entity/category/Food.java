6c007086d6a6fcc8e04178ec64c5a7c97ae19cf7
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2026-01-26 03:03:54 +0900

    feat: 자식(FOOD) 생성 및 필드 수정 메서드 추가

--- src/main/java/com/lhj/myoppingmall/item/entity/category/Food.java
+++ src/main/java/com/lhj/myoppingmall/item/entity/category/Food.java
@@ -1,5 +1,6 @@
 package com.lhj.myoppingmall.item.entity.category;
 
+import com.lhj.myoppingmall.item.dto.update.FoodUpdateDto;
 import com.lhj.myoppingmall.item.entity.Item;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
@@ -20,4 +21,36 @@ public class Food extends Item {
     private String manufacturerCompany;
     private LocalDate expireDate;
     private String description;
+
+
+    /*
+    * 자녀 생성 메서드
+    * */
+    public static Food create(
+            Category category,
+            String name,
+            Long price,
+            String pictureUrl,
+            Integer stockQuantity,
+            String manufacturerCompany,
+            LocalDate expireDate,
+            String description
+    ) {
+        Food food = new Food();
+        food.setCommon(category, name, price, pictureUrl, stockQuantity);
+        food.manufacturerCompany = manufacturerCompany;
+        food.expireDate = expireDate;
+        food.description = description;
+        return food;
+    }
+
+    /*
+     * 자녀 필드 수정 메서드
+     * */
+    public void changeDetails(FoodUpdateDto dto) {
+        if(dto == null) return;
+        if(dto.getManufacturerCompany() != null) this.manufacturerCompany = dto.getManufacturerCompany();
+        if(dto.getExpireDate() != null) this.expireDate = dto.getExpireDate();
+        if(dto.getDescription() != null) this.description = dto.getDescription();
+    }
 }
