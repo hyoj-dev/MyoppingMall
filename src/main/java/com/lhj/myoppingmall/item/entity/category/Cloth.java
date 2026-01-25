3fe5e8135a51901af785c374b04321a0ce51438f
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2026-01-26 03:05:45 +0900

    feat: 자녀(CLOTH) 생성 및 필드 수정 메서드 추가

--- src/main/java/com/lhj/myoppingmall/item/entity/category/Cloth.java
+++ src/main/java/com/lhj/myoppingmall/item/entity/category/Cloth.java
@@ -1,5 +1,6 @@
 package com.lhj.myoppingmall.item.entity.category;
 
+import com.lhj.myoppingmall.item.dto.update.ClothUpdateDto;
 import com.lhj.myoppingmall.item.entity.Item;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
@@ -18,4 +19,36 @@ public class Cloth extends Item {
     private String size;
     private String brand;
     private String description;
+
+
+    /*
+    * 자녀 생성 메서드
+    * */
+    public static Cloth create(
+            Category category,
+            String name,
+            Long price,
+            String pictureUrl,
+            Integer stockQuantity,
+            String size,
+            String brand,
+            String description
+    ) {
+        Cloth cloth = new Cloth();
+        cloth.setCommon(category, name, price, pictureUrl, stockQuantity);
+        cloth.size = size;
+        cloth.brand = brand;
+        cloth.description = description;
+        return cloth;
+    }
+
+    /*
+     * 자녀 수정 메서드
+     * */
+    public void changeDetails(ClothUpdateDto dto) {
+        if(dto == null) return;
+        if(dto.getSize() != null) this.size = dto.getSize();
+        if(dto.getBrand() != null) this.brand = dto.getBrand();
+        if(dto.getDescription() != null) this.description = dto.getDescription();
+    }
 }
