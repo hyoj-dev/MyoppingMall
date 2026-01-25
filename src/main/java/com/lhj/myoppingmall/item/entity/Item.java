085a121536de34f18a20738c6e89149e90babf27
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2026-01-26 03:02:24 +0900

    feat: Item 관련 편의 메서드 추가 (onCreate, Item 공통 필드 설정 메서드, Item 필드 공통 수정 메서드)

--- src/main/java/com/lhj/myoppingmall/item/entity/Item.java
+++ src/main/java/com/lhj/myoppingmall/item/entity/Item.java
@@ -35,4 +35,34 @@ public abstract class Item {
     private LocalDateTime registAt;
     private LocalDateTime registerAt;
 
+    //==편의 메서드==
+
+    /*
+    * save 시점 자동 호출
+    * */
+    @PrePersist
+    protected void onCreate() {
+        this.registerAt = LocalDateTime.now();
+    }
+
+    /*
+    * 공통 필드 설정 메서드
+    * */
+    public void setCommon(Category category, String name, Long price, String pictureUrl, Integer stockQuantity) {
+        this.category = category;
+        this.name = name;
+        this.price = price;
+        this.pictureUrl = pictureUrl;
+        this.stockQuantity = stockQuantity;
+    }
+
+    /*
+     * 공통 수정 메서드
+     * */
+    public void changeCommon(String name, Long price, String pictureUrl, Integer stockQuantity) {
+        if (name != null) this.name = name;
+        if (price != null) this.price = price;
+        if (pictureUrl != null) this.pictureUrl = pictureUrl;
+        if (stockQuantity != null) this.stockQuantity = stockQuantity;
+    }
 }
