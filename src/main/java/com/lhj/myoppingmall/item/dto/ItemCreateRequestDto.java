6a7e4d2a4fbf28fc2dc7af08c63c2872e0c6ec93
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2026-01-26 03:09:33 +0900

    feat: CreateItem(상품 등록) 위한 Dto 추가

---
+++ src/main/java/com/lhj/myoppingmall/item/dto/ItemCreateRequestDto.java
@@ -0,0 +1,32 @@
+package com.lhj.myoppingmall.item.dto;
+
+import com.lhj.myoppingmall.item.entity.category.Category;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+public class ItemCreateRequestDto {
+    private Category category;
+    private String name;
+    private Long price;
+    private String pictureUrl;
+    private int stockQuantity;
+
+    //Cloth
+    private String size;
+    private String brand;
+
+    //Food
+    private LocalDate expireDate;
+
+    //Electronic_Device
+    private int warrantyMonths;
+
+    //Food & Electronic_Device
+    private String manufacturerCompany;
+
+    //Cloth & Food & Electronic_Device
+    private String description;
+
+}
