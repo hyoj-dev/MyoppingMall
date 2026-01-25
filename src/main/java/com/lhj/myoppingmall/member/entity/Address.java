06059390070cc1ead8b105ec2927da070579c91a
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2026-01-26 02:49:52 +0900

    refactor: PROTECTED no-arg 생성자 적용

--- src/main/java/com/lhj/myoppingmall/member/entity/Address.java
+++ src/main/java/com/lhj/myoppingmall/member/entity/Address.java
@@ -1,10 +1,15 @@
 package com.lhj.myoppingmall.member.entity;
 
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Address {
 
     private String city;
