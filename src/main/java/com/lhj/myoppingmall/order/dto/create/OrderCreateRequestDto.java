4440c6018758e78ef5eed03f13caabff003f60ab
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2026-02-05 02:04:05 +0900

    refactor: OrderCreateRequestDto에 테스트용 Builder 추가

--- src/main/java/com/lhj/myoppingmall/order/dto/create/OrderCreateRequestDto.java
+++ src/main/java/com/lhj/myoppingmall/order/dto/create/OrderCreateRequestDto.java
@@ -1,13 +1,15 @@
 package com.lhj.myoppingmall.order.dto.create;
 
 import com.lhj.myoppingmall.order.dto.OrderItemDto;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
-@NoArgsConstructor
+//@NoArgsConstructor
+@Builder
 public class OrderCreateRequestDto {
     private List<OrderItemDto> orderItems;
 }
