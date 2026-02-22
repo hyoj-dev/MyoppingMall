e711de2cc9bce1ecb6f99c5d2729090de356a5b1
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2026-02-22 21:20:55 +0900

    refactor: Member에 관한 Security Filter 구조 변경

--- src/main/java/com/lhj/myoppingmall/global/config/SecurityConfig.java
+++ src/main/java/com/lhj/myoppingmall/global/config/SecurityConfig.java
@@ -32,8 +32,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/error").permitAll()
                         .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/members/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/members").permitAll()
+                        .requestMatchers("/members/me/**").authenticated()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(
